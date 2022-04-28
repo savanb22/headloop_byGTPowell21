117b3cf009b2bed042c91e792b35877348a07ec2
savanb22 <104596708+savanb22@users.noreply.github.com>
2022-04-29 01:28:33 +0200

    Update example.py

--- example.py
+++ example.py
@@ -21,5 +21,8 @@ output = design(tbx16_AA_F, tbx16_AA_R, tbx16_AA_guide, tbx16_AA_guide_orientati
 #Output of the function: note that 'design' returns two primers as SeqRecord objects, with comments on how closely matched
 #the predicated annealing temperatures are. Only one of those primers should be used, to be determined by testing.
 #In this individual case, the antisense headloop primer was used for HL-PCR.
-print(' Sense headloop primer:', output[0].seq, '\n', output[0].description, '\n', 'Antisense headloop primer:', output[1].seq, '\n', output[1].description)
-print('success_repeat')
+print('tbx16_AA_F',
+      'tbx16_AA_R',
+      ' Sense headloop primer:', output[0].seq, '\n', output[0].description, '\n', 
+      'Antisense headloop primer:', output[1].seq, '\n', output[1].description)
+print('success_repeat3')
