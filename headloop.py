05ff8ca468205ab6439673c5611389d1c778fd1d
savanb22 <104596708+savanb22@users.noreply.github.com>
2022-04-29 00:09:53 +0200

    Update headloop.py

--- headloop.py
+++ headloop.py
@@ -60,6 +60,7 @@ Returns:
     
 """
 
+from Bio.Seq import Seq
 import Seq #library from biopython GitHub repository [https://github.com/biopython/biopython/blob/master/Bio/Seq.py]
 import SeqRecord #library from from biopython GitHub repository [https://github.com/biopython/biopython/blob/master/Bio/SeqRecord.py]
 import melting #library from Erik Clarke's GitHub repository [https://github.com/eclarke/melt]
