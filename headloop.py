d18aef9f2832ad230f63d1c8220d4e93fd2eb00c
savanb22 <104596708+savanb22@users.noreply.github.com>
2022-04-29 00:46:39 +0200

    Update headloop.py

--- headloop.py
+++ headloop.py
@@ -67,7 +67,8 @@ from Bio.SeqRecord import SeqRecord
 
 import melting #library from Erik Clarke's GitHub repository [https://github.com/eclarke/melt]
 
-def design(sense_oligo, antisense_oligo, guide_context, orientation):
+def design('CTGGTCCAGTGCGTTATTGG', 'AGCCAAATGCTTCTTGCTCTTTT', 'CTACAGGACGTACCTGCACCCGGATTCACCAGCGCCCG', 'antisense'):
+#def design(sense_oligo, antisense_oligo, guide_context, orientation):
 
     #identify strand for Tm comparison and tagging   
     orient_error = 'Orientation not correctly specified.\n'
